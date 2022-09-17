fc6e15b3d52735245f7d3fad81140988e1458d1f
kimigop1827 <113401505+kimigop1827@users.noreply.github.com>
2022-09-17 11:27:52 +0800

    testbot

---
+++ README.md
@@ -0,0 +1,10 @@
+{}BOT share by HChong
+It reported an error, let it run often BOT 
+# This is a BOT for BOX, emphasizing that BOX can be used, then use this File for interactive BOX Ping, Check,...
+link Facebook: https://www.facebook.com/profile.php?id=100052097501614
+Zalo: 0354500378
+Support reg bot, common mistakes, fix fb link name in modules with reg bot's fb link
+You can donate at:
+Momo: 0354500378
+Sacombank: 0602 7033 9002
+File share should be commands guys
---
+++ login.js
@@ -0,0 +1,53 @@
+const fs = require("fs-extra");
+const login = require('desuni');
+const readline = require("readline");
+const totp = require("totp-generator");
+
+let configPath = "";
+let argv = process.argv.slice(2);
+if (argv.length !== 0) configPath = argv[0];
+else configPath = "./config.json";
+
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+const option = {
+	logLevel: "silent",
+	forceLogin: true,
+	userAgent: "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 OPR/84.0.4316.21"
+};
+
+const config = require(`./${configPath}`);
+let email = ;
+let password = ;
+let otpkey = config.OTPKEY.replace(/\s+/g, '').toLowerCase();
+
+login({ email, password }, option, (err, api) => {
+	if (err) {
+		switch (err.error) {
+			case "login-approval":
+				if (otpkey) err.continue(totp(otpkey));
+				else {
+					console.log("Enter the 2-layer verification code:");
+					rl.on("line", line => {
+						err.continue(line);
+						rl.close();
+					});
+				}
+				break;
+			default:
+			console.error(err);
+			process.exit(1);
+		}
+		return;
+	}
+	const json = JSON.stringify(api.getAppState());
+	fs.writeFileSync(`./${config.APPSTATE}`, json);
+	console.log("Finished recording fbstate!");
+	process.exit(0);
+});
+Đã ghi xong appstate!");
+	process.exit(0);
+});
